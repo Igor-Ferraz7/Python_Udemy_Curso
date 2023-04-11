ae4bb0d6e9c83a0f4088ac01490542fe47d90fbc
Igor-Ferraz7 <igorsousaferrazaraga2@gmail.com>
2023-04-11 17:03:24 -0300

    Introdução à listas.

---
+++ Aulas/aula86(resolucao).py
@@ -0,0 +1,4 @@
+lista = ['a', 'b', 'c', 'd']
+indices = range(len(lista))  #obs: começou pelo zero e foi ate 3, indicando dessa forma que há 4 indices na lista.
+for indice in indices:
+    print(indice, lista[indice])
---
+++ Aulas/aula87.py
@@ -0,0 +1,11 @@
+# desempacotamento de listas
+lista = [1, 2]
+num1, num2 = lista
+print(num1, num2)
+
+str1, str2 = ['ola', 'opa']
+print(str1, str2)
+
+lista2 = [0, 1, 2, 3, 4, 5]
+_, num1, *_ = lista2 # "_" é usado para guardar variavies que não serão utilizadas, como se fosse uma lixeira.
+print(num1, _)
