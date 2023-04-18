7e4a9b8738b40dc8f01e626c2ff7b6fdf62066ac
Igor-Ferraz7 <igorsousaferrazaraga2@gmail.com>
2023-04-18 14:14:52 -0300

    Últimos exercícios de laços de repetição.

--- Aulas/Seção_Básica/aula70(ex).py
+++ Aulas/Seção_Básica/aula70(ex).py
@@ -14,14 +14,11 @@ for key, value in dicionario.items():
     if contador == 1:
         if value > last_value:
             biggest_value = value
-            letra_moda = key #onde parei: salvando a letra que mais aparece (é preciso lidar dps se duas letras tiverem o mesmo tanto de vezes aparecida.)
+            letra_moda = key 
 
         elif value < last_value:
             biggest_value = last_value
             letra_moda = key
-        
-        # elif value == biggest_value:
-        #     ...
 
     else:
         if value > biggest_value:
@@ -33,9 +30,7 @@ for key, value in dicionario.items():
             letra_moda = key
 
     last_value = value
-    # lista.append(value)
 
-# lista.sort()
 print(f'Letra: {letra_moda} Qtd: {biggest_value}')
 
 
--- Aulas/Seção_Básica/aula70(resolucao).py
+++ Aulas/Seção_Básica/aula70(resolucao).py
@@ -6,7 +6,6 @@ maior_qtd_letra = 0
 while contador < len(frase):
     letra_atual = frase[contador]
     qtd_letra = frase.count(letra_atual)
-    # print(f"{letra_atual}: {qtd_letra}")
 
     if ' ' in letra_atual:
         contador += 1
