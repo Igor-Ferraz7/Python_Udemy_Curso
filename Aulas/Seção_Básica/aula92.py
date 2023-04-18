fcda4a8666188c9acd2c7a82557666ccfbf118ad
Igor-Ferraz7 <igorsousaferrazaraga2@gmail.com>
2023-04-18 14:21:28 -0300

    Imprecisão dos números flutuantes.

--- Aulas/Seção_Básica/aula92.py
+++ Aulas/Seção_Básica/aula92.py
@@ -1,8 +1,9 @@
-# impresição dos números de ponto fluuante:
+# impresição dos números de ponto flutuante:
 import decimal
 
 n1 = decimal.Decimal('0.1')
 n2 = decimal.Decimal('0.7')
 n3 = n1 + n2
+
 print(n3) #retorna 0.79999... ao invés de 0.8 (sem o modulo 'decimal') / retorna 0.8 com o modulo decimal
 print(round(n3, 1)) #corrige o erro sem modulos
